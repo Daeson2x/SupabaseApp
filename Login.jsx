f90780522b39da334ba73ea36b317a8310be39f9
Denilson Santana <denilson.santana@alumnos.udg.mx>
2024-10-21 23:13:51 -0600

    login dostres

--- Login.jsx
+++ Login.jsx
@@ -1,108 +1,105 @@
-import React, {Component} from 'react';
-import {View, Text, Image, TextInput, Button, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text , TextInput , ImageBackground, StyleSheet, Dimensions, ScrollView, Button, Alert} from 'react-native';
+
+const { height } = Dimensions.get('window');
 
 export default class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      codigo: '',
-      nip: '',
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            usuario:"",
+            contra: "",
+        };
+    }
 
-  render() {
-    const login = () => {
-      console.log('entro');
-      let _this = this;
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          // Typical action to be performed when the document is ready:
-          console.log(xhttp.responseText);
-          
-          if (!xhttp.responseText === '0') {
-            //error
-          } else {
-            _this.props.navigation.navigate('drawer',{codigo:_this.state.codigo,nip:_this.state.nip});
-          }
+    render() {
+        const log = () =>{
+            
+            if (this.state.usuario === "" || this.state.contra === "") {
+                Alert.alert("Error al iniciar sesion","Asegurese que las credenciales sean correctas o esten llenos los campos")
+                
+            }
+            else{
+                //Falta validacion de existencia del usuario
+                Alert.alert("Inicio correcto",`Bienvenido ${this.state.usuario}`, )
+            }
+
+
+            this.setState({
+                usuario: "",
+                contra: ""
+            });
         }
-      };
-      xhttp.open(
-        'GET',
-        'http://148.202.152.33/cucei/credenciales.php?codigo=' +
-          this.state.codigo +
-          '&nip=' +
-          this.state.nip,
-        true,
-      );
-      xhttp.send();
-    };
-    return (
-      <View>
-        
-        <View style={styles.imageContainer}>
-          
+        return (
+        <View style={style.container}>
+            <ScrollView >
+                <ImageBackground  source={require('./images/fondo.jpg')} resizeMode="cover" style={style.image} >
+                <View style={style.inputView}>
+                    <View style={style.viewLabel}>
+                        <Text style={style.label}>
+                            Usuario 
+                        </Text>
+                    </View>
+                    <TextInput style={style.input} placeholderTextColor={"gray"}  placeholder='Ingresa tu usuario' value={this.state.usuario} onChangeText={usuario => this.setState({usuario})}/>
+                    <View style={style.viewLabel}>
+                        <Text style={style.label}>
+                            Contraseña 
+                        </Text>
+                    </View>
+                    <TextInput style={style.input} placeholderTextColor={"gray"} placeholder='Ingresa tu contraseña' secureTextEntry={true} value={this.state.contra} onChangeText={contra => this.setState({contra})}/>
+                    <View style={style.viewButton}>
+                        <Button title='Ingresar' onPress={log} color={"black"} />
+                    </View>
+                    
+                </View>
+            </ImageBackground>
+            </ScrollView>
+            
         </View>
-        <View style={styles.inputsContainer}>
-          <View
-          style={{
-            borderRadius: 15,
-            borderColor: 'black',
-            borderWidth: 3,
-            width: '45%',
-          }}>
-          <TextInput
-            style={{color:'black'}}
-            placeholder="Codigo"
-            placeholderTextColor="gray"
-            onChangeText={codigo => this.setState({codigo})}
-          />
-          </View>
-        </View>
-
-        <View style={styles.inputsContainer}>
-          <View
-            style={{
-              borderRadius: 15,
-              borderColor: 'black ',
-              borderWidth: 3,
-              width: '45%',
-            }}>
-            <TextInput
-              style={{color:'black'}}
-              placeholder="nip"
-              placeholderTextColor="gray"
-              onChangeText={nip => this.setState({nip})}
-              secureTextEntry={true}
-            />
-          </View>
-        </View>
-        
-
-        <View style={{alignItems:'center'}}>
-          <View
-            style={{
-              borderRadius: 9,
-              borderColor: 'black',
-              width: '45%',
-              marginTop:'5%'
-            }}>
-            <Button title="entrar" onPress={login} />
-          </View>
-        </View>
-        
-      </View>
-    );
-  }
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  imageContainer:{
-    alignItems: "center",
-    top:15,
-  },
-  inputsContainer:{
-    alignItems:'center',  
-    marginTop:'10%'
-  }
+const style = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+    image:{
+        flex:1,
+        alignItems:"center",
+        width:"100%",
+        height:height,
+    },
+    inputView:{
+        backgroundColor:"rgba(255, 255, 255, 0.9)",
+        width: "85%",
+        height:400,
+        marginTop:"35%",
+        borderRadius:10,
+        alignItems:"center"
+    }, 
+    input: {
+        height: 50,
+        width:"80%",
+        margin:"1%",
+        borderWidth: 1,
+        padding: 10,
+        fontSize:18,
+        borderRadius:5,
+        color:"black"
+    },
+    viewLabel:{
+        width:"100%"
+    },
+    label:{
+        padding:"3%",
+        fontSize:30,
+        textAlign:"left",
+        fontStyle:"italic",
+        color:"black"
+    },
+    viewButton:{
+        marginTop:"7%",
+        width:"80%",
+    },
 })
